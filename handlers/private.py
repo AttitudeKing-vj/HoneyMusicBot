a960cc561147bb979cd6d3ac7b651dca266735a5
Vijay <95028968+AttitudeKing-vj@users.noreply.github.com>
2022-11-26 21:29:52 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -12,7 +12,7 @@ from config import BOT_NAME, BOT_USERNAME
 from config import BOT_NAME
 from config import BOT_USERNAME
 
-@Client.on_message(command("start") & filters.private & ~filters.edited)
+@Client.on_message(command("music") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"https://te.legra.ph/file/bb20e1b39e8d7a31f7cbc.jpg",
